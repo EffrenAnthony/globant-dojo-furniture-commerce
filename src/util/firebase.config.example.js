cafc792c0c68dd2e9279836b546ba8a8f4e7e9f8
Tony <effrenanthony2704@gmail.com>
2023-02-09 23:30:07 -0500

    including firebase authentincation

--- src/util/firebase.config.example.js
+++ src/util/firebase.config.example.js
@@ -1,21 +1,15 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
-// import {getAnalytics} from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmho7tA9VhKgTZrTZCxxjsLMnVdiQiv1k',
-  authDomain: 'globant-dojo-firebase.firebaseapp.com',
-  projectId: 'globant-dojo-firebase',
-  storageBucket: 'globant-dojo-firebase.appspot.com',
-  messagingSenderId: '662482433969',
-  appId: '1:662482433969:web:d8f107b94f6e2ee4e42f90',
-  measurementId: 'G-0YD94PSE2V',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
